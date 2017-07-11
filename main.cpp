f57b6d2d18ddcd9edc5bf7f35affe7ba6344ba8f
Jesus Fernandez <jsfdez@gmail.com>
2017-07-11 16:07:49 +0200

    Initial commit

---
+++ main.cpp
@@ -0,0 +1,36 @@
+#include <QGuiApplication>
+#include <QOpenGLWindow>
+#include <QOpenGLFunctions>
+#include <QTimer>
+
+class OpenGLWindow : public QOpenGLWindow
+{
+public:
+    void paintGL()
+    {
+        const auto rand = qrand() / float(RAND_MAX);
+        qDebug("paint called %f", rand);
+        context()->functions()->glClearColor(qrand() / float(RAND_MAX),
+                                             qrand() / float(RAND_MAX),
+                                             qrand() / float(RAND_MAX),
+                                             1.f);
+        context()->functions()->glClear(GL_COLOR_BUFFER_BIT);
+    }
+};
+
+int main(int argc, char *argv[])
+{
+    QGuiApplication a(argc, argv);
+    OpenGLWindow w;
+    w.show();
+
+    QTimer timer;
+    QObject::connect(&timer, &QTimer::timeout, &w, QOverload<>::of(&QOpenGLWindow::update));
+    timer.setSingleShot(false);
+    timer.start(1000);
+
+    MessageBox(
+            HWND(w.winId()), (LPCWSTR)L"TEXT", (LPCWSTR)L"CAPTION", MB_ICONINFORMATION);
+
+    return a.exec();
+}
---
+++ use_hwnd.pro
@@ -0,0 +1,13 @@
+QT       += core gui opengl
+
+TARGET = use_hwnd
+TEMPLATE = app
+
+LIBS += -lUser32
+
+SOURCES += \
+        main.cpp
+
+HEADERS +=
+
+FORMS +=
